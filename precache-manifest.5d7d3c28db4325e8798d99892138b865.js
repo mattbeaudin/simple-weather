3d33feee6d7377f8b4a7c08b6f52dfd6a2709575
Matt Beaudin <beaudinm@msu.edu>
2020-07-29 18:47:54 -0400

    Updates

---
+++ precache-manifest.5d7d3c28db4325e8798d99892138b865.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a094c8512e9531344a21bc7208389ee",
+    "url": "/simple-weather/index.html"
+  },
+  {
+    "revision": "e712d6126c508160815e",
+    "url": "/simple-weather/static/css/2.84e92681.chunk.css"
+  },
+  {
+    "revision": "6d92cbdacdd7bb6bd36a",
+    "url": "/simple-weather/static/css/main.9d1a79c7.chunk.css"
+  },
+  {
+    "revision": "e712d6126c508160815e",
+    "url": "/simple-weather/static/js/2.6b978392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-weather/static/js/2.6b978392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d92cbdacdd7bb6bd36a",
+    "url": "/simple-weather/static/js/main.d341c83b.chunk.js"
+  },
+  {
+    "revision": "99039606ee1ac868bf01",
+    "url": "/simple-weather/static/js/runtime-main.4b74cb1c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/simple-weather/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/simple-weather/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/simple-weather/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/simple-weather/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/simple-weather/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
