a1fd19d68e1340e6705113f8e4688eb19b9e3a6a
Matt Beaudin <beaudinm@msu.edu>
2020-08-02 02:01:10 -0400

    Updates

---
+++ precache-manifest.8e83e0370db1e300975c81f463d9d980.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a4dd7e456451762d826d740563c4862",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00a53d729b9a02a4dc8b",
+    "url": "/static/css/2.de4a0485.chunk.css"
+  },
+  {
+    "revision": "2ac3dbf01af89dd67ab7",
+    "url": "/static/css/main.8954ed83.chunk.css"
+  },
+  {
+    "revision": "00a53d729b9a02a4dc8b",
+    "url": "/static/js/2.7b4e19e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7b4e19e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ac3dbf01af89dd67ab7",
+    "url": "/static/js/main.9c1082d0.chunk.js"
+  },
+  {
+    "revision": "d7efe1b8ac3af1fec4a9",
+    "url": "/static/js/runtime-main.778c8aaf.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
